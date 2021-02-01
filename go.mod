d8b103e3657049c6d9a2fef4fefefd133cf195d7
Driaan <debeste.driaan@gmail.com>
2021-02-01 11:18:03 +0200

    update save

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/driaan/nodeppt-manager
+
+go 1.15
+
+require (
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.6.3
+)
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type HTMLFormDetails struct {
+	Message string
+}
+
+func preRunTasks() {
+
+}
+
 func main() {
-	
+	preRunTasks()
+	router := gin.Default()
+	router.LoadHTMLGlob("web/*")
+	router.GET("/", func(c *gin.Context) {
+		details := HTMLFormDetails{
+			Message: "hello",
+		}
+		c.HTML(http.StatusOK, "index.html", details)
+	})
+	router.StaticFS("/css", http.Dir("/assets/css"))
+	router.StaticFS("/js", http.Dir("/assets/js"))
+	router.Use(cors.Default())
+	router.Run(":80")
 }
